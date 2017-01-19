d14b03682a41f1ad12c04b367fc8d8995e90ea67
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2017-01-19 12:05:58 +0100

    WIP

--- ubuntu/server-16.04/config.sh
+++ ubuntu/server-16.04/config.sh
@@ -14,3 +14,12 @@ function install() {
   mecho "Installing $@"
   sudo apt-get install -y $@
 }
+
+function backup_file () {
+  local timestamp=`date +%Y%m%d%H_%M_%S`
+  local file=$1
+  local backup="${file}.bkp_${timestamp}"
+
+  mecho "Backing up file ${file} to ${backup}"
+  cp -rp $file $backup
+}
--- ubuntu/server-16.04/nginx-passenger.sh
+++ ubuntu/server-16.04/nginx-passenger.sh
@@ -58,3 +58,23 @@ else
 fi
 
 chruby $RUBY_VERSION
+
+
+echo "Installing Passenger"
+
+# https://www.phusionpassenger.com/library/install/nginx/install/oss/xenial/
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
+sudo apt-get install -y apt-transport-https ca-certificates
+
+# Add our APT repository
+sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger xenial main > /etc/apt/sources.list.d/passenger.list'
+sudo apt-get update
+
+# Install Passenger + Nginx
+sudo apt-get install -y nginx-extras passenger
+
+
+backup_file /etc/nginx/nginx.conf
+sed -i 's/# \(include \/etc\/nginx\/passenger\.conf\.*\)/\1/g' /etc/nginx/nginx.conf
+
+sudo /usr/bin/passenger-config validate-install --auto
