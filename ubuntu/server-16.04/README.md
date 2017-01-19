4b06bd1b09850c6f861686ed2c15e6cd6daef565
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2017-01-19 09:12:20 +0100

    WIP

---
+++ ubuntu/server-16.04/README.md
@@ -0,0 +1,8 @@
+Nginx + Phuson Passenger on Ubuntu 16.04 Server
+---------------
+
+```
+ENV
+```
+
+- [InstallingPhuson Passenger](https://www.phusionpassenger.com/library/install/nginx/install/oss/xenial/)
---
+++ ubuntu/server-16.04/nginx-passenger.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+# Works on Ubuntu server 16.4
+
+#
+# Configuration
+#-------------------------------------------------------------------------------
+
+RUBY_VERSION='2.4.0'
+
+
+#-------------------------------------------------------------------------------
+
+set -e
+source "`dirname $0`/config.sh"
+alias echo='mecho'
+
+install git
+install nginx
+
+
+#
+# Ruby installation
+#-------------------------------------------------------------------------------
+
+echo "Installing ruby-install"
+
+wget -O /opt/ruby-install-0.6.1.tar.gz https://github.com/postmodern/ruby-install/archive/v0.6.1.tar.gz
+cd /opt
+tar -xzvf ruby-install-0.6.1.tar.gz
+cd ruby-install-0.6.1/
+make install
+ruby-install
+ruby-install ruby $RUBY_VERSION
+
+
+#
+# Ruby configuration
+#-------------------------------------------------------------------------------
+
+echo "Installing chruby"
+
+wget -O /opt/chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz
+cd /opt
+tar -xzvf chruby-0.3.9.tar.gz
+cd chruby-0.3.9/
+make install
+PROFILE_CHRUBY=/etc/profile.d/chruby.sh
+
+if [ "$CHRUBY_VERSION"]; then
+  echo ''
+else
+  source /usr/local/share/chruby/chruby.sh
+  echo 'if [ -n "$BASH_VERSION" ] || [ -n "$ZSH_VERSION" ]; then' >  $PROFILE_CHRUBY
+  echo '  source /usr/local/share/chruby/chruby.sh'               >> $PROFILE_CHRUBY
+  echo 'fi'                                                       >> $PROFILE_CHRUBY
+  echo "chruby $RUBY_VERSION"                                     >> $PROFILE_CHRUBY
+fi
+
+chruby $RUBY_VERSION
