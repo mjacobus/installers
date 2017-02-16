a4a37702bfab7b819d5ebb716aa8c46257f6683e
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2017-02-16 10:03:22 +0100

    Added dev tools

--- apple/sierra/misc.sh
+++ apple/sierra/misc.sh
@@ -1,16 +1,3 @@
 #!/usr/bin/env bash
 
-brew install mysql
-brew install ossp-uuid
-
-brew tap universal-ctags/universal-ctags
-brew install --HEAD universal-ctags
-
-
-brew install the_silver_searcher
-brew install tmux
-brew install reattach-to-user-namespace
-brew install curl
-brew install phantomjs
-brew install wget
 brew install mplayer
