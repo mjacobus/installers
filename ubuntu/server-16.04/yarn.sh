236a8bbe5b2d0b822067b86f0df452cb76104a41
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2017-06-14 22:39:45 +0200

    Yarn installation

---
+++ ubuntu/server-16.04/yarn.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+source "`dirname $0`/config.sh"
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt-get update
+
+install yarn
