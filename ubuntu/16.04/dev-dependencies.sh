6ee36ba91db03f461ce6473d2d0ca04212ae5f03
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2017-08-24 20:23:52 +0200

    Improve installers

--- ubuntu/16.04/dev-dependencies.sh
+++ ubuntu/16.04/dev-dependencies.sh
@@ -4,6 +4,15 @@ set -e
 
 source "`dirname $0`/config.sh"
 
+install openssh-server
+install git
+install tmux
 install curl
 install build-essential patch
 install ruby-dev zlib1g-dev liblzma-dev
+install libxml2-dev
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt-get update && sudo apt-get install yarn
