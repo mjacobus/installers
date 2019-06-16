2175e448f4293f02ab598769ce88129aeb258c91
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2019-06-16 12:02:22 -0300

    Add installers for ubuntu 19.4

---
+++ ubuntu/19.4/installer.sh
@@ -0,0 +1,13 @@
+alias install='sudo apt-get install -y'
+
+sudo apt-get -y install zsh
+sudo apt-get -y install git
+sudo apt-get -y install tmux
+sudo apt-get -y install vim-gnome
+sudo apt-get -y install curl
+sudo apt-get -y install net-tools
+sudo apt-get -y install silversearcher-ag
+# xclip -sel clip < ~/.ssh/id_rsa.pub
+
+
+
