7e2ddcde977042748edd94d690067068428d1490
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2019-06-26 14:51:08 -0300

    Add script for installing docker on ubuntu 19.4

---
+++ ubuntu/19.4/docker.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# https://medium.com/@Grigorkh/how-to-install-docker-on-ubuntu-19-04-7ccfeda5935
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Either
+# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable edge"
+
+# or
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable edge test"
+
+
+sudo apt-get update
+apt-cache policy docker-ce
+sudo apt-get install -y docker-ce
+sudo systemctl status docker
+sudo usermod -aG docker ${USER}
