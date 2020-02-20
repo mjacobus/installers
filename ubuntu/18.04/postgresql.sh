5770292446e615646ceff9b043cd4ccf0272a06f
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2020-02-20 18:34:59 -0300

    Add one more step to postgres installation

--- ubuntu/18.04/postgresql.sh
+++ ubuntu/18.04/postgresql.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 # https://hostadvice.com/how-to/how-to-install-postgresql-database-server-on-ubuntu-18-04/
+# https://www.digitalocean.com/community/tutorials/how-to-use-postgresql-with-your-ruby-on-rails-application-on-ubuntu-18-04
 
 set -ex
 
 sudo apt-get install -y postgresql postgresql-contrib
 
 sudo -u postgres psql -c "SELECT version();"
+
+sudo -u postgres createuser -s `whoami` -P
