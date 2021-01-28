c914e5104d11b678ff754d2516d7b158eaf53118
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-01-28 17:17:27 -0300

    Add installation scripts for bigsur

---
+++ osx/bigsur/install.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -ev
+
+brew install tmux
+brew install reattach-to-user-namespace
+brew install jq
+brew install htop
+brew install mplayer
+brew install the_silver_searcher
+brew tap universal-ctags/universal-ctags
+brew install --HEAD universal-ctags
+brew install gpg
+
+
+brew install wget
+brew install watch
+
+# newovim
+brew install python3
+brew install neovim/neovim/neovim
+pip3 install neovim
+
+brew install tig
+
+# ruby dependencies
+# brew install ossp-uuid
+# brew install mysql
+# brew install cmake
+# brew install libtool
+# brew install zmq
+# brew install shellcheck
+# brew install fzf
+# brew install coreutils
+# brew install gpg
+# brew install automake
