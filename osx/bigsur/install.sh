25837be75d22d4509019603a8430d0c2d4dcc370
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-01-30 19:12:14 -0300

    Update bigsur script

--- osx/bigsur/install.sh
+++ osx/bigsur/install.sh
@@ -11,29 +11,14 @@ brew install the_silver_searcher
 brew tap universal-ctags/universal-ctags
 brew install --HEAD universal-ctags
 brew install gpg
-
-
 brew install vim --override-system-vim
-
-
 brew install wget
 brew install watch
-
-# newovim
 brew install python3
 brew install neovim/neovim/neovim
 pip3 install neovim
-
 brew install tig
+brew install taglib
 
-# ruby dependencies
-# brew install ossp-uuid
-# brew install mysql
-# brew install cmake
-# brew install libtool
-# brew install zmq
-# brew install shellcheck
-# brew install fzf
-# brew install coreutils
-# brew install gpg
-# brew install automake
+# GH Depencency
+
