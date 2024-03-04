a71a613151812dbab7db454276ff91ceb8a60797
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-03-03 22:35:36 -0300

    More stuff

---
+++ ubuntu/22.04-lts/installers/base.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+sudo apt install -y stow
+sudo apt install -y git
+sudo apt install -y curl
+sudo apt install -y zsh
--- ubuntu/22.04-lts/vagrant-provision.sh
+++ ubuntu/22.04-lts/vagrant-provision.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
+./installers/base.sh
 ./installers/ubuntu-desktop.sh
 ./installers/brazilian-timezone.sh
+./installers/neovim.sh
+./installers/lvim.sh
+
+# sudo apt install -y docker
+# sudo apt install -y docker-compose
+# sudo apt install -y stuff to compile stuff
+
 
 echo "Setting the timezone to America/Sao_Paulo..."
 sudo timedatectl set-timezone America/Sao_Paulo
