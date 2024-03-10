d678f1e9154868cfcd6f59be6d3e6759839e6783
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-03-10 20:31:54 -0300

    Install dependencies

--- ubuntu/22.04-lts/installers/base.sh
+++ ubuntu/22.04-lts/installers/base.sh
@@ -4,3 +4,20 @@ sudo apt install -y stow
 sudo apt install -y git
 sudo apt install -y curl
 sudo apt install -y zsh
+
+sudo apt-get install -y  build-essential
+sudo apt-get install -y libssl-dev
+sudo apt-get install -y zlib1g-dev
+sudo apt-get install -y libbz2-dev
+sudo apt-get install -y libreadline-dev
+sudo apt-get install -y libsqlite3-dev
+sudo apt-get install -y wget
+sudo apt-get install -y curl
+sudo apt-get install -y llvm
+sudo apt-get install -y libncurses5-dev
+sudo apt-get install -y libncursesw5-dev
+sudo apt-get install -y xz-utils
+sudo apt-get install -y tk-dev
+sudo apt-get install -y libffi-dev
+sudo apt-get install -y liblzma-dev
+sudo apt-get install -y python-openssl
