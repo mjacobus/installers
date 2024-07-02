e5e65257cd67549a12e96f6c73167473c934c048
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-07-02 17:52:06 -0300

    Skip lvim installation

--- ubuntu/22.04-lts/installers/neovim.sh
+++ ubuntu/22.04-lts/installers/neovim.sh
@@ -5,6 +5,6 @@
 sudo apt-get uninstall neovim
 # sudo snap install --edge nvim --classic
 
-sudo snap install nvim --classic
+# sudo snap install nvim --classic
 
-sudo apt install -y python3-neovim
+# sudo apt install -y python3-neovim
--- ubuntu/22.04-lts/vagrant-provision.sh
+++ ubuntu/22.04-lts/vagrant-provision.sh
@@ -6,12 +6,11 @@ sudo apt-get update
 ./installers/ubuntu-desktop.sh
 ./installers/brazilian-timezone.sh
 ./installers/neovim.sh
-./installers/lvim.sh
+# ./installers/lvim.sh
 
 # sudo apt install -y docker
 # sudo apt install -y docker-compose
 # sudo apt install -y stuff to compile stuff
-
 
 echo "Setting the timezone to America/Sao_Paulo..."
 sudo timedatectl set-timezone America/Sao_Paulo
