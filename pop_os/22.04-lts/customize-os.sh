37b9205c8ff16052239cb382977a9be17982576a
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2025-12-10 16:21:54 -0300

    Pop os stuff

---
+++ pop_os/22.04-lts/customize-os.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+set -e
+
+# -----------------------------
+# Disable animations (no workspace slide)
+# -----------------------------
+gsettings set org.gnome.desktop.interface enable-animations false
+
+# -----------------------------
+# Workspaces: Super+1..9, Super+0 (10)
+# -----------------------------
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1  "['<Super>1']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2  "['<Super>2']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3  "['<Super>3']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4  "['<Super>4']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5  "['<Super>5']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6  "['<Super>6']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-7  "['<Super>7']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-8  "['<Super>8']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-9  "['<Super>9']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-10 "['<Super>0']"
+
+# -----------------------------
+# Move window to workspace: Super+Shift+N
+# -----------------------------
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-1  "['<Super><Shift>1']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-2  "['<Super><Shift>2']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-3  "['<Super><Shift>3']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-4  "['<Super><Shift>4']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-5  "['<Super><Shift>5']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-6  "['<Super><Shift>6']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-7  "['<Super><Shift>7']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-8  "['<Super><Shift>8']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-9  "['<Super><Shift>9']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-10 "['<Super><Shift>0']"
+
+# -----------------------------
+# Remove left/right workspace navigation (i3-like)
+# -----------------------------
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left  "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "[]"
+
+# -----------------------------
+# Close window with Super+w
+# -----------------------------
+gsettings set org.gnome.desktop.wm.keybindings close "['<Super>w']"
+
+# -----------------------------
+# Remove Super overview conflict
+# -----------------------------
+gsettings set org.gnome.shell.keybindings toggle-overview "[]"
+
+echo "✅ GNOME / Pop!_OS keybindings configured successfully."
+
---
+++ pop_os/22.04-lts/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# sudo apt-get update
+sudo apt install build-essential rustc libssl-dev libyaml-dev zlib1g-dev libgmp-dev
+sudo apt install tmux
+sudo apt install htop
